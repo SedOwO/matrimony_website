f48a06c9f0186d5f6fbe8ab139c6075d8670cb44
Surabhi <surabhi@fiddlers-MacBook-Pro.local>
2025-01-16 19:42:20 +0530

    user schema update

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -9,16 +9,12 @@ import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// Create or Initialize Profile
 router.post('/', protect, completeProfile);
 
-// Get Profile by User ID
 router.get('/:userId', protect, getProfile);
 
-// Update Profile
 router.put('/:userId', protect, updateProfile);
 
-// Delete Profile
 router.delete('/:userId', protect, deleteProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 
@@ -13,12 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', userRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/admin', adminRoutes);
 
 app.listen(5000, () => {
-    
     console.log("Server started at http://localhost:5000");
 });
 
